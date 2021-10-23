9f1f7557a394fcfb081882b3adf26feadd8cc85d
Agnieszka Dutka <agud100@gmail.com>
2021-10-23 23:08:30 +0200

    rabbits procreate - state_plans to redesign

--- examples/rabbits/lib/rabbits/plan_creator.ex
+++ examples/rabbits/lib/rabbits/plan_creator.ex
@@ -7,7 +7,6 @@ defmodule Rabbits.PlanCreator do
 
   @impl true
   defn create_plan(i, j, plans, grid, object_data, iteration) do
-    # TODO rabbits and lettuce create state_plan as well!
     plan =
       cond do
         Nx.equal(grid[i][j][0], @rabbit) ->
@@ -25,14 +24,51 @@ defmodule Rabbits.PlanCreator do
   end
 
   defnp create_plan_rabbit(i, j, grid, object_data) do
+    cond do
+      Nx.less(object_data[i][j], 1) ->
+        rabbit_die()
 
-        # TODO all rabbits go up when signal is 0
-        # TODO procreate and die depending on energy
-        if Nx.equal(object_data[i][j], 0) do
-            Nx.tensor([@dir_stay, @remove_rabbit, @keep])
+      Nx.greater(object_data[i][j], 10) ->
+        rabbit_procreate(grid, i, j)
+
+      :otherwise ->
+        rabbit_move(grid, i, j)
+    end
+  end
+
+  defnp rabbit_die() do
+    Nx.tensor([@dir_stay, @remove_rabbit, @keep])
+  end
+
+  defnp rabbit_procreate(grid, i, j) do
+    {_i, _j, _direction, availability, availability_size, _grid} =
+      while {i, j, direction = @dir_top, availability = Nx.broadcast(Nx.tensor(0), {8}), curr = 0,
+             grid},
+            Nx.less_equal(direction, @dir_top_left) do
+        {x, y} = shift({i, j}, direction)
+
+        if Nx.equal(grid[x][y][0], @empty) or Nx.equal(grid[x][y][0], @lettuce) do
+          availability = Nx.put_slice(availability, [curr], Nx.broadcast(direction, {1}))
+          {i, j, direction + 1, availability, curr + 1, grid}
         else
+          {i, j, direction + 1, availability, curr, grid}
+        end
+      end
+
+    if availability_size > 0 do
+      index = Nx.random_uniform({1}, 0, availability_size, type: {:s, 8})
+      direction = Nx.reshape(availability[index], {1})
+      action_consequence = Nx.tensor([@add_rabbit, @keep])
+      Nx.concatenate([direction, action_consequence])
+    else
+      Nx.tensor([@dir_stay, @keep, @keep])
+    end
+  end
+
+  defnp rabbit_move(grid, i, j) do
     {_i, _j, _direction, signals, _grid} =
-      while {i, j, direction = @dir_top, signals = Nx.broadcast(Nx.tensor(-@infinity), {9}), grid},
+      while {i, j, direction = @dir_top, signals = Nx.broadcast(Nx.tensor(-@infinity), {9}),
+             grid},
             Nx.less_equal(direction, @dir_top_left) do
         {x, y} = shift({i, j}, direction)
 
@@ -57,14 +93,12 @@ defmodule Rabbits.PlanCreator do
     end
   end
 
-  end
-
-  # TODO refer to direction as 1 or @top?
   defnp create_plan_lettuce(i, j, grid, iteration) do
     if Nx.remainder(iteration, @lettuce_growth_factor) |> Nx.equal(Nx.tensor(0)) do
       {_i, _j, _direction, availability, availability_size, _grid} =
-        while {i, j, direction = 1, availability = Nx.broadcast(Nx.tensor(0), {8}), curr = 0, grid},
-              Nx.less(direction, 9) do
+        while {i, j, direction = @dir_top, availability = Nx.broadcast(Nx.tensor(0), {8}),
+               curr = 0, grid},
+              Nx.less(direction, @dir_top_left) do
           {x, y} = shift({i, j}, direction)
 
           if Nx.equal(grid[x][y][0], @empty) do
@@ -74,17 +108,18 @@ defmodule Rabbits.PlanCreator do
             {i, j, direction + 1, availability, curr, grid}
           end
         end
-        # TODO fire in EVACUATION will error when av size == 0
-        if availability_size > 0 do
-          index = Nx.random_uniform({1}, 0, availability_size, type: {:s, 8})
 
-          # to create [dir, mock, empty] we convert dir (scalar tensor) to tensor of shape [1]
-          direction = Nx.reshape(availability[index], {1})
-          action_consequence = Nx.tensor([@add_lettuce, @keep])
-          Nx.concatenate([direction, action_consequence])
-        else
-          Nx.tensor([@dir_stay, @keep, @keep])
-        end
+      # TODO fire in EVACUATION will error when av size == 0
+      if availability_size > 0 do
+        index = Nx.random_uniform({1}, 0, availability_size, type: {:s, 8})
+
+        # to create [dir, mock, empty] we convert dir (scalar tensor) to tensor of shape [1]
+        direction = Nx.reshape(availability[index], {1})
+        action_consequence = Nx.tensor([@add_lettuce, @keep])
+        Nx.concatenate([direction, action_consequence])
+      else
+        Nx.tensor([@dir_stay, @keep, @keep])
+      end
     else
       Nx.tensor([@dir_stay, @keep, @keep])
     end
--- examples/rabbits/lib/rabbits/plan_resolver.ex
+++ examples/rabbits/lib/rabbits/plan_resolver.ex
@@ -6,21 +6,12 @@ defmodule Rabbits.PlanResolver do
   import Simulator.Helpers
 
   @impl true
-  # TODO why?
   defn is_update_valid?(action, object) do
     cond do
       both_equal(action, @add_lettuce, object, @empty) -> Nx.tensor(1)
       both_equal(action, @add_rabbit, object, @empty) -> Nx.tensor(1)
       both_equal(action, @add_rabbit, object, @lettuce) -> Nx.tensor(1)
       both_equal(action, @remove_rabbit, object, @rabbit) -> Nx.tensor(1)
-      # both_equal(action, @add_person, object, @fire) -> Nx.tensor(1)
-
-      # both_equal(action, @remove_person, object, @person) -> Nx.tensor(1)
-
-      # both_equal(action, @create_fire, object, @empty) -> Nx.tensor(1)
-      # both_equal(action, @create_fire, object, @person) -> Nx.tensor(1)
-      # both_equal(action, @create_fire, object, @exit) -> Nx.tensor(1)
-
       true -> Nx.tensor(0)
     end
   end
@@ -28,19 +19,28 @@ defmodule Rabbits.PlanResolver do
   @impl true
   defn apply_update(grid, object_data, x, y, action, object, old_state) do
     cond do
-      both_equal(action, @add_lettuce, object, @empty) -> {put_object(grid, x, y, @lettuce), put_state(object_data, x, y, old_state)}
-      both_equal(action, @add_rabbit, object, @empty) ->
-        {put_object(grid, x, y, @rabbit), put_state(object_data, x, y, old_state - 1) }
-      both_equal(action, @add_rabbit, object, @lettuce) ->
-        {put_object(grid, x, y, @rabbit), put_state(object_data, x, y, old_state + 1) }
-      both_equal(action, @remove_rabbit, object, @rabbit) -> {put_object(grid, x, y, @empty), object_data}
+      both_equal(action, @add_lettuce, object, @empty) ->
+        do_update(grid, object_data, x, y, @lettuce, old_state)
 
-      true -> {grid, object_data}
+      both_equal(action, @add_rabbit, object, @empty) ->
+        do_update(grid, object_data, x, y, @rabbit, old_state - 1)
+
+      both_equal(action, @add_rabbit, object, @lettuce) ->
+        do_update(grid, object_data, x, y, @rabbit, old_state + 1)
+
+      both_equal(action, @remove_rabbit, object, @rabbit) ->
+        do_update(grid, object_data, x, y, @empty, object_data[x][y])
+
+      true ->
+        {grid, object_data}
     end
+  end
+
+  defnp do_update(grid, object_data, x, y, new_object, new_state) do
+    {put_object(grid, x, y, new_object), put_state(object_data, x, y, new_state)}
   end
 
   defnp put_state(object_data, i, j, state) do
     Nx.put_slice(object_data, [i, j], Nx.broadcast(state, {1, 1}))
   end
-
 end
