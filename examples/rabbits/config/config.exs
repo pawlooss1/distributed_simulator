4dd5759d5f4d7d656f77a30e072b602c77b5525c
sheldak <samuel.heldak@swmansion.com>
2021-11-22 23:54:28 +0100

    update config

--- examples/rabbits/config/config.exs
+++ examples/rabbits/config/config.exs
@@ -2,6 +2,6 @@ import Config
 
 config :distributed_simulator,
   module_prefix: Rabbits,
-  max_iterations: 1,
+  max_iterations: 25,
   signal_suppression_factor: 0.6,
   signal_attenuation_factor: 0.6
