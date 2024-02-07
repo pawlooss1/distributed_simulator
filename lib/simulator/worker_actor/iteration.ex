34b07f29902b1b52dc7aeff0385544dc24536efb
pawlooss1 <pawlooss13@gmail.com>
2024-02-07 16:14:07 +0100

    Only calculating signals

--- lib/simulator/worker_actor/iteration.ex
+++ lib/simulator/worker_actor/iteration.ex
@@ -29,31 +29,32 @@ defmodule Iteration do
          generate_signal,
          signal_factor
        ) do
-    plans = Plans.create_plans_2(iteration, grid, objects_state, rng, create_plan)
+    # plans = Plans.create_plans_2(iteration, grid, objects_state, rng, create_plan)
 
-    {order, rng} = Nx.Random.shuffle(rng, Nx.tensor([0, 1, 2, 3, 5, 6, 7, 8]))
+    # {order, rng} = Nx.Random.shuffle(rng, Nx.tensor([0, 1, 2, 3, 5, 6, 7, 8]))
 
-    {updated_grid, accepted_plans, updated_objects_state} =
-      Plans.process_plans_2(
-        grid,
-        plans,
-        objects_state,
-        order,
-        is_update_valid?,
-        apply_action
-      )
+    # {updated_grid, accepted_plans, updated_objects_state} =
+    #   Plans.process_plans_2(
+    #     grid,
+    #     plans,
+    #     objects_state,
+    #     order,
+    #     is_update_valid?,
+    #     apply_action
+    #   )
 
-    {updated_grid, updated_objects_state} =
-      Consequences.apply_consequences_2(
-        updated_grid,
-        updated_objects_state,
-        plans,
-        accepted_plans,
-        apply_consequence
-      )
+    # {updated_grid, updated_objects_state} =
+    #   Consequences.apply_consequences_2(
+    #     updated_grid,
+    #     updated_objects_state,
+    #     plans,
+    #     accepted_plans,
+    #     apply_consequence
+    #   )
 
-    # signal_update = Signal.calculate_signal_updates(updated_grid, generate_signal)
+    signal_update = Signal.calculate_signal_updates(grid, generate_signal)
     # final_grid = Signal.apply_signal_update(updated_grid, signal_update, signal_factor)
-    {updated_grid, updated_objects_state, rng}
+    # {final_grid, updated_objects_state, rng}
+    signal_update
   end
 end
